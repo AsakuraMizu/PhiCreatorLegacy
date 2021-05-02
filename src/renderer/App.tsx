e9c606b20f0dbe9baddf4117e9f72e734496ceb5
AsakuraMizu <0xWATERx0@gmail.com>
2021-05-02 22:09:59 +0800

    chore: remove debug

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,11 +8,10 @@ import {
   ThemeProvider,
 } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
-import { control, project } from './managers';
+import { control } from './managers';
 import Hotkeys from './components/Hotkeys';
 import Toast from './components/Toast';
 import Editor from './components/Editor';
-// import FullViewer from './components/FullViewer';
 import FooterBar from './components/FooterBar';
 import theme from './theme';
 import Preview from './components/Preview';
@@ -38,13 +37,6 @@ const useStyles = makeStyles(() => ({
 
 export default observer(function App() {
   const cn = useStyles();
-
-  React.useEffect(() => {
-    api.project
-      .openProject('/home/waterl/dasein')
-      .then(() => project.reload(false, true))
-      .then(() => project.mark(false));
-  }, []);
 
   return (
     <ThemeProvider theme={theme}>
